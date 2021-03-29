af3108c8466a0e9d5a529130630c9b210ca2a5cc
Chau Nguyen Duy Toan <chaunguyenduytoan@gmail.com>
2021-03-29 23:00:41 +0700

    First commit

---
+++ app/src/main/java/com/example/dolaapp/Entities/User.java
@@ -0,0 +1,96 @@
+package com.example.dolaapp.Entities;
+
+import java.util.List;
+
+public class User {
+    protected String UserEmail;
+    protected String UserPassword;
+    protected String UserPhone;
+    protected String UserName;
+    protected String UserDoB;
+
+    protected List<String> UserConversation;
+    protected List<String> requestSend;
+    protected List<String> listRequest;
+    protected List<String> listFriend;
+
+    public User(String userEmail, String userPassword, String userPhone, String userName, String userDoB) {
+        UserEmail = userEmail;
+        UserPassword = userPassword;
+        UserPhone = userPhone;
+        UserName = userName;
+        UserDoB = userDoB;
+    }
+
+    public String getUserEmail() {
+        return UserEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        UserEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return UserPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        UserPassword = userPassword;
+    }
+
+    public String getUserPhone() {
+        return UserPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        UserPhone = userPhone;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getUserDoB() {
+        return UserDoB;
+    }
+
+    public void setUserDoB(String userDoB) {
+        UserDoB = userDoB;
+    }
+
+    public List<String> getUserConversation() {
+        return UserConversation;
+    }
+
+    public void setUserConversation(List<String> userConversation) {
+        UserConversation = userConversation;
+    }
+
+    public List<String> getRequestSend() {
+        return requestSend;
+    }
+
+    public void setRequestSend(List<String> requestSend) {
+        this.requestSend = requestSend;
+    }
+
+    public List<String> getListRequest() {
+        return listRequest;
+    }
+
+    public void getListRequest(List<String> listrequest) {
+        this.listRequest = listrequest;
+    }
+
+    public List<String> getListFriend() {
+        return listFriend;
+    }
+
+    public void setListFriend(List<String> listfriend) {
+        this.listFriend = listfriend;
+    }
+}
---
+++ app/src/main/java/com/example/dolaapp/LoginScreenActivity.java
@@ -0,0 +1,41 @@
+package com.example.dolaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+
+public class LoginScreenActivity extends AppCompatActivity {
+    EditText usernameEditText, passwordEditText;
+    Button loginButton,forgotPassword,signUp;
+    ImageButton info;
+    ProgressBar loadingProgressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+        initControls();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingProgressBar.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+
+    protected void initControls(){
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login);
+        forgotPassword = findViewById(R.id.forgotPassword);
+        signUp = findViewById(R.id.signUp);
+        info = findViewById(R.id.info);
+        loadingProgressBar = findViewById(R.id.loading);
+    }
+}
